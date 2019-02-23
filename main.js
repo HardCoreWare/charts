9935f5bb6bf19cb00e5c620a739c212b5336f69b
HardCoreWare <bernardocodu@gmail.com>
2019-02-22 19:19:30 -0600

    listo

--- main.js
+++ main.js
@@ -146,7 +146,7 @@ let data = [{label:"one",values:[1,1.5]},{label:"two",values:[2,2.5]}];
 let labels = ["dataset 1","ddos"];
 
 
-const singleChart = new SingleChart('line',2);
+const singleChart = new SingleChart('bar',2);
 singleChart.setData(data,labels);
 singleChart.begin("myChart");
 
@@ -158,6 +158,6 @@ let node={
 
 }
 
-//singleChart.push(node);
+singleChart.push(node);
 //console.log(singleChart.pop());
 //singleChart.unshift(node);
