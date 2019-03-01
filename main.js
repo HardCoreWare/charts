db7ed3c0d97613987286d09b298f44d4ecdae656
HardCoreWare <bernardocodu@gmail.com>
2019-02-28 21:28:33 -0600

    separando main de clase principal

--- main.js
+++ main.js
@@ -1,172 +1,3 @@
-class SingleChart{
-
-    //
-    constructor(type,size){
-
-        this.colors = [
-            {
-                backgroundColor:'rgba(255, 99, 132, 0.2)',
-                borderColor:'rgba(255, 99, 132, 0.2)'
-            },
-            {
-                backgroundColor:'rgba(132, 99, 255, 0.2)',
-                borderColor:'rgba(132, 99, 255, 0.2)'
-            },
-            {
-                backgroundColor:'rgba(255, 99, 132, 0.2)',
-                borderColor:'rgba(255, 99, 132, 0.2)'
-            },
-            {
-                backgroundColor:'rgba(255, 99, 132, 0.2)',
-                borderColor:'rgba(255, 99, 132, 0.2)'
-            },
-            {
-                backgroundColor:'rgba(255, 99, 132, 0.2)',
-                borderColor:'rgba(255, 99, 132, 0.2)'
-            },
-            {
-                backgroundColor:'rgba(255, 99, 132, 0.2)',
-                borderColor:'rgba(255, 99, 132, 0.2)'
-            }
-        ];
-
-        this.type = type;
-        this.size = size;
-        this.chart = null;
-        this.ctx = null;
-        this.data = {
-
-            labels: [],
-            datasets: []
-        }
-
-    }
-
-    //
-    begin(divId){
-
-        this.ctx = document.getElementById(divId).getContext('2d');
-        this.chart = new Chart(this.ctx, {
-            type: this.type,
-            data: this.data,
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
-                }
-            }
-        });
-
-    }
-
-    //
-    end(){
-
-        this.ctx.destroy();
-        this.chart.destroy();
-
-    }
-
-    //
-    setData(data,labels){
-
-        //iteramos etiquetas en X
-        data.forEach(node => {
-
-            this.data.labels.push(node.label);
-            
-        });
-
-        for (let i = 0; i < this.size; i++) {
-
-            let dataset = {
-
-                label: labels[i],
-                data:[],
-                backgroundColor:this.colors[i].backgroundColor,
-                borderColor:this.colors[i].borderColor,
-                borderWidth: 1
-
-
-            };
-
-            data.forEach(node=>{
-
-                let value=node.values[i];
-
-                dataset.data.push(value);
-
-            });
-
-            this.data.datasets.push(dataset);
-    
-        }
-
-    }
-
-    //
-    push(node){
-
-        //
-        this.data.labels.push(node.label);
-
-        //
-        for (let i = 0; i < this.size; i++) {
-
-            this.data.datasets[i].data.push(node.values[i]);
-            
-        }
-
-        this.chart.update();
-
-    }
-
-    unshift(node){
-
-        //
-        this.data.labels.unshift(node.label);
-
-        //
-        for (let i = 0; i < this.size; i++) {
-
-            this.data.datasets[i].data.unshift(node.values[i]);
-            
-        }
-
-        this.chart.update();
-
-    }
-
-    //
-    pop(){
-
-        let node={
-
-            label:this.data.labels.pop(),
-
-            values:[]
-
-        }
-
-        //
-        for (let i = 0; i < this.size; i++) {
-
-            node.values[i]=this.data.datasets[i].data.pop();
-            
-        }
-
-        this.chart.update();
-
-        return(node);
-
-    }
-
-}
-
-
 let data = [{label:"one",values:[1,1.5]},{label:"two",values:[2,2.5]}];
 let labels = ["dataset 1","ddos"];
 
@@ -174,7 +5,6 @@ let labels = ["dataset 1","ddos"];
 const singleChart = new SingleChart('bar',2);
 singleChart.setData(data,labels);
 singleChart.begin("myChart");
-
 
 let node={
 
@@ -184,5 +14,11 @@ let node={
 }
 
 singleChart.push(node);
+singleChart.push(node);
+singleChart.push(node);
+singleChart.push(node);
+singleChart.push(node);
+singleChart.push(node);
+
 //console.log(singleChart.pop());
 //singleChart.unshift(node);
