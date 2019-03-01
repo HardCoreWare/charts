f329fcc972bc617056e4b301bd7caa0872b87d35
HardCoreWare <bernardocodu@gmail.com>
2019-02-28 22:02:21 -0600

    separando main de clase principal

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 let data = [{label:"one",values:[1,1.5]},{label:"two",values:[2,2.5]}];
 let labels = ["dataset 1","ddos"];
 
-
 const singleChart = new SingleChart('bar',2);
 singleChart.setData(data,labels);
 singleChart.begin("myChart");
 
-let node={
+document.getElementById("btnPush").onclick=function () {
 
-    label : "three",
-    values : [3,3.5]
-
+    alert("cool");
+    
 }
-
-//console.log(singleChart.pop());
-//singleChart.unshift(node);
